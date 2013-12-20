55323d67a2905056474fb252b3d8cc8552f3ac40
karones <jexp@yandex.ru>
2013-12-20 11:49:31 +0600

    Create mss_to_connect.pl

---
+++ mss_to_connect.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $k; 
+my $tcp_dump=system 'tcpdump -i any -vv -c 100 host st.perm.ertelecom.ru -l|tee tcpdump.log &';
+sleep 2;
+my $p=`iperf -c st.perm.ertelecom.ru -t1 `;
+sleep 10;
+open (my $fh, '<./tcpdump.log');
+while (<$fh>){
+        if ($_=~m/Flags \[S.\],/){
+            if ($_=~m/\[mss\s+(.*?),/) {
+                   $k=$1;
+        }
+}
+}
+
+print ("$k\n");
+close ($fh);
