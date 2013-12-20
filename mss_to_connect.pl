dcaf0f2a30fac22edec01d92358e6caecfb1f1d6
karones <jexp@yandex.ru>
2013-12-20 12:07:27 +0600

    Update mss_to_connect.pl

--- mss_to_connect.pl
+++ mss_to_connect.pl
@@ -6,7 +6,7 @@ my $k;
 my $tcp_dump=system 'tcpdump -i any -vv -c 100 host st.perm.ertelecom.ru -l|tee tcpdump.log &';
 sleep 2;
 my $p=`iperf -c st.perm.ertelecom.ru -t1 `;
-sleep 10;
+sleep 2;
 open (my $fh, '<./tcpdump.log');
 while (<$fh>){
         if ($_=~m/Flags \[S.\],/){
